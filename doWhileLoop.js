674017e98bc6b61e9f070f51e823f11d37ae3ba7
Leland Byrd <lbyrd@truecoders.io>
2023-01-13 14:16:32 -0600

    fixed do while loop prompts

--- doWhileLoop.js
+++ doWhileLoop.js
@@ -1,19 +1,18 @@
-// let base = parseInt(
-//     ("What number do you want to a multiplication table of?")
-//   );
+let base = parseInt(
+    prompt("What number do you want to a multiplication table of?")
+  );
 
-//   if (!base) {
-//       alert("you have not selected a number, we will make it 1")
-//     base = 1;
-//   }
+  if (!base) {
+      alert("you have not selected a number, we will make it 1")
+    base = 1;
+  }
 
-//   let range = parseInt(prompt("What number should I stop at?"));
-//   if (!range) {
-//       alert("you have not selected a number, we will make it 10")
-//     range = 10;
-//   }
-let base = 8;
-let range = 10
+  let range = parseInt(prompt("What number should I stop at?"));
+  if (!range) {
+      alert("you have not selected a number, we will make it 10")
+    range = 10;
+  }
+
 let i = 1;
 
 do {
