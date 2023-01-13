e154884e4fec96dff677470dd5299e9afff1ec39
Leland Byrd <lbyrd@truecoders.io>
2023-01-13 10:48:54 -0600

    created edge case protections on whileLoop multiplication example with alert messages

--- whileLoop.js
+++ whileLoop.js
@@ -1,10 +1,15 @@
 let base = parseInt(
   prompt("What number do you want to a multiplication table of?")
 );
-let range = parseInt(prompt("What number should I stop at?"));
-
 if (!base) {
+    alert("you have not selected a number, we will make it 1")
   base = 1;
+}
+
+let range = parseInt(prompt("What number should I stop at?"));
+if (!range) {
+    alert("you have not selected a number, we will make it 10")
+  range = 10;
 }
 
 let i = 1;
